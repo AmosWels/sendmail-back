70e09d3f38970ab66e41d9f959faa9380b95b899
Amos Welike <amoswelike@gmail.com>
2020-06-24 15:33:31 +0300

    finish backend app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_mail import Mail, Message
 from flask_cors import CORS
 
@@ -14,14 +14,21 @@ app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 
 mail = Mail(app)
+# 'martinkatamba@gmail.com',
 
-@app.route('/sendmail')
+@app.route('/sendmail', methods=['POST'])
 def index():
+    if request.method=='POST':
+        posted_data = request.get_json()
+        data = posted_data['data']
+        name = data['name']
+        price = data['price']
+        email = data['email']
     try:
-        msg = Message('Hello', sender = 'doep40077@gmail.com', recipients = ['martinkatamba@gmail.com', 'amoswelike@gmail.com'])
-        msg.body = "This is the email body for testing"
+        msg = Message('Hello customer care', sender = 'doep40077@gmail.com', recipients = ['amoswelike@gmail.com', email])
+        msg.body = "This is %s \n and i would like agro-chemicals at a price of %s" % (name, price)
         mail.send(msg)
-        return "Your email has been Sent"
+        return jsonify({"message":"Your email has been Sent"})
     except Exception as e:
         return(str(e))
 
